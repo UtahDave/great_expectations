1106c2c1d77207e52a243cc6c98a0bc20de995f6
Chetan Kini <chetan@superconductive.com>
2021-08-18 11:10:34 -0400

    test: Implement Python test

--- tests/integration/docusaurus/connecting_to_your_data/cloud/pandas_azure_python_example/configured_asset_azure_data_connector.py
+++ tests/integration/docusaurus/connecting_to_your_data/cloud/pandas_azure_python_example/configured_asset_azure_data_connector.py
@@ -1,26 +1,42 @@
+import os
+
 from ruamel import yaml
 
 import great_expectations as ge
 from great_expectations.core.batch import BatchRequest, RuntimeBatchRequest
 
+ACCOUNT_URL = os.getenv("AZURE_ACCOUNT_URL", "")
+CREDENTIAL = os.getenv("AZURE_CREDENTIAL", "")
+
 context = ge.get_context()
 
 datasource_config = {
-    "name": "my_s3_datasource",
+    "name": "my_azure_datasource",
     "class_name": "Datasource",
-    "execution_engine": {"class_name": "PandasExecutionEngine"},
+    "execution_engine": {
+        "class_name": "PandasExecutionEngine",
+        "azure_options": {
+            "account_url": "<YOUR_ACCOUNT_URL>",
+            "credential": "<YOUR_CREDENTIAL>",
+        },
+    },
     "data_connectors": {
         "default_runtime_data_connector_name": {
             "class_name": "RuntimeDataConnector",
             "batch_identifiers": ["default_identifier_name"],
         },
-        "default_inferred_data_connector_name": {
-            "class_name": "InferredAssetS3DataConnector",
-            "bucket": "<YOUR_S3_BUCKET_HERE>",
-            "prefix": "<BUCKET_PATH_TO_DATA>",
+        "default_configured_data_connector_name": {
+            "class_name": "ConfiguredAssetAzureDataConnector",
+            "azure_options": {
+                "account_url": "<YOUR_ACCOUNT_URL>",
+                "credential": "<YOUR_CREDENTIAL>",
+            },
+            "container": "superconductive-public",
+            "name_starts_with": "data/taxi_yellow_trip_data_samples/",
+            "assets": {"taxi_data": None},
             "default_regex": {
-                "group_names": ["data_asset_name"],
-                "pattern": "(.*)\\.csv",
+                "pattern": "data/taxi_yellow_trip_data_samples/yellow_trip_data_sample_(\\d{4})-(\\d{2})\\.csv",
+                "group_names": ["year", "month"],
             },
         },
     },
@@ -28,11 +44,19 @@ datasource_config = {
 
 # Please note this override is only to provide good UX for docs and tests.
 # In normal usage you'd set your path directly in the yaml above.
-datasource_config["data_connectors"]["default_inferred_data_connector_name"][
-    "bucket"
+datasource_config["execution_engine"]["azure_options"]["account_url"] = ACCOUNT_URL
+datasource_config["execution_engine"]["azure_options"]["credential"] = CREDENTIAL
+datasource_config["data_connectors"]["default_configured_data_connector_name"][
+    "azure_options"
+]["account_url"] = ACCOUNT_URL
+datasource_config["data_connectors"]["default_configured_data_connector_name"][
+    "azure_options"
+]["credential"] = CREDENTIAL
+datasource_config["data_connectors"]["default_configured_data_connector_name"][
+    "container"
 ] = "superconductive-public"
-datasource_config["data_connectors"]["default_inferred_data_connector_name"][
-    "prefix"
+datasource_config["data_connectors"]["default_configured_data_connector_name"][
+    "name_starts_with"
 ] = "data/taxi_yellow_trip_data_samples/"
 
 context.test_yaml_config(yaml.dump(datasource_config))
@@ -41,10 +65,12 @@ context.add_datasource(**datasource_config)
 
 # Here is a RuntimeBatchRequest using a path to a single CSV file
 batch_request = RuntimeBatchRequest(
-    datasource_name="my_s3_datasource",
+    datasource_name="my_azure_datasource",
     data_connector_name="default_runtime_data_connector_name",
     data_asset_name="<YOUR_MEANGINGFUL_NAME>",  # this can be anything that identifies this data_asset for you
-    runtime_parameters={"path": "<PATH_TO_YOUR_DATA_HERE>"},  # Add your S3 path here.
+    runtime_parameters={
+        "path": "<PATH_TO_YOUR_DATA_HERE>"
+    },  # add your Azure path here.
     batch_identifiers={"default_identifier_name": "something_something"},
 )
 
@@ -52,14 +78,16 @@ batch_request = RuntimeBatchRequest(
 # In normal usage you'd set your path directly in the BatchRequest above.
 batch_request.runtime_parameters[
     "path"
-] = "s3a://superconductive-public/data/taxi_yellow_trip_data_samples/yellow_trip_data_sample_2019-01.csv"
+] = f"{ACCOUNT_URL}/superconductive-public/data/taxi_yellow_trip_data_samples/yellow_trip_data_sample_2018-01.csv"
 
 context.create_expectation_suite(
     expectation_suite_name="test_suite", overwrite_existing=True
 )
+
 validator = context.get_validator(
     batch_request=batch_request, expectation_suite_name="test_suite"
 )
+
 print(validator.head())
 
 # NOTE: The following code is only for testing and can be ignored by users.
@@ -67,16 +95,14 @@ assert isinstance(validator, ge.validator.validator.Validator)
 
 # Here is a BatchRequest naming a data_asset
 batch_request = BatchRequest(
-    datasource_name="my_s3_datasource",
-    data_connector_name="default_inferred_data_connector_name",
+    datasource_name="my_azure_datasource",
+    data_connector_name="default_configured_data_connector_name",
     data_asset_name="<YOUR_DATA_ASSET_NAME>",
 )
 
 # Please note this override is only to provide good UX for docs and tests.
 # In normal usage you'd set your data asset name directly in the BatchRequest above.
-batch_request.data_asset_name = (
-    "data/taxi_yellow_trip_data_samples/yellow_trip_data_sample_2019-01"
-)
+batch_request.data_asset_name = "taxi_data"
 
 context.create_expectation_suite(
     expectation_suite_name="test_suite", overwrite_existing=True
@@ -88,13 +114,9 @@ print(validator.head())
 
 # NOTE: The following code is only for testing and can be ignored by users.
 assert isinstance(validator, ge.validator.validator.Validator)
-assert [ds["name"] for ds in context.list_datasources()] == ["my_s3_datasource"]
+assert [ds["name"] for ds in context.list_datasources()] == ["my_azure_datasource"]
 assert set(
-    context.get_available_data_asset_names()["my_s3_datasource"][
-        "default_inferred_data_connector_name"
+    context.get_available_data_asset_names()["my_azure_datasource"][
+        "default_configured_data_connector_name"
     ]
-) == {
-    "data/taxi_yellow_trip_data_samples/yellow_trip_data_sample_2019-01",
-    "data/taxi_yellow_trip_data_samples/yellow_trip_data_sample_2019-02",
-    "data/taxi_yellow_trip_data_samples/yellow_trip_data_sample_2019-03",
-}
+) == {"taxi_data"}
